e8ec38d4f303544718ce5d4333b1ea2d47fe2e9b
kseniia <kseniia@smartiot.cloud>
2022-12-04 01:27:50 +0200

    upload data to db script

--- src/scripts/uploadDataToDb.js
+++ src/scripts/uploadDataToDb.js
@@ -2,14 +2,16 @@ const {db} = require('../../firebase-config.js');
 const {ref, set} = require('firebase/database');
 const fs = require('fs');
 const path = require('path');
+const {fetchAddressesByNames} = require('./fetchAddressesByNames.js');
 
-function writeUserData() {
+async function writeUserData() {
   const file = fs.readFileSync(
-    path.resolve(__dirname, '../assets/universities.json'),
+    path.resolve(__dirname, '../../assets/universities.json'),
     'utf-8',
   );
   const universities = JSON.parse(file);
-  set(ref(db, 'universities'), universities);
+  const universitiesWithCitiesName = await fetchAddressesByNames(universities);
+  set(ref(db, 'universities'), universitiesWithCitiesName);
 }
 
 writeUserData();
